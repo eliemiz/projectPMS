92df1d10f574456b480366bce9947a7c927a9203
Heisely <ypd00062@gmail.com>
2021-03-30 23:43:49 +0900

    calendar 컨트롤러, 뷰

---
+++ src/pms/a01_controller/A14_CalendarController.java
@@ -0,0 +1,21 @@
+package pms.a01_controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pms.a02_service.A14_CalendarService;
+
+@Controller
+@RequestMapping("calendar.do")
+public class A14_CalendarController {
+	@Autowired(required = false)
+	private A14_CalendarService service;
+	
+	// http://localhost:7080/projectPMS/calendar.do?method=list
+	@GetMapping(params = "method=list")
+	public String CalendarList() {
+		return "a14_calendar\\a01_calendar";
+	}
+}
