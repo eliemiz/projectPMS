dc6ee43dfe120e900b8a6e17cc53c12ea6576bd0
chaeyoung_lee <www970630@gmail.com>
2021-04-02 23:37:02 +0900

    admin

--- src/pms/a01_controller/A02_AdminController.java
+++ src/pms/a01_controller/A02_AdminController.java
@@ -12,7 +12,7 @@ public class A02_AdminController {
 	@Autowired(required = false)
 	private A02_AdminService service;
 	
-	// http:localhost:7080/projectPMS/admin.do
+	// http://localhost:7080/projectPMS/admin.do
 	@RequestMapping
 	public String main() {
 		return "a02_admin\\a00_adminMain";
