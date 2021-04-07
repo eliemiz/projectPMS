827e2e4e581025c00fdc4293f3dc373e5a04e3ba
Isukim <aftersoul1994@gmail.com>
2021-04-07 19:02:06 +0900

    notice test 수정

--- src/pms/a01_controller/A15_NoticeController.java
+++ src/pms/a01_controller/A15_NoticeController.java
@@ -1,11 +1,15 @@
 package pms.a01_controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pms.a02_service.A15_NoticeService;
+import pms.z01_vo.Notice;
 
 @Controller
 @RequestMapping("notice.do")
@@ -15,10 +19,21 @@ public class A15_NoticeController {
 	
 	// http://localhost:6080/projectPMS/notice.do?method=list
 	// http://localhost:7080/projectPMS/notice.do?method=list
-	@GetMapping(params="method=list")
-	public String NoticeList() {
+	@RequestMapping
+	public String Notice_list(@ModelAttribute("sch") Notice sch, Model d) {
+		
+		d.addAttribute("noticeList", service.getNoticeList(sch));
+		
 		return "a15_notice\\a01_notice";
 	}
+	/*
+	@GetMapping(params="method=list")
+	public String NoticeList(Model d) {
+		ArrayList<Notice> noticeList = service.getNoticeList();
+		
+		d.addAttribute("noticeList", noticeList);
+		return "a15_notice\\a01_notice";
+	}*/
 	// http://localhost:6080/projectPMS/notice.do?method=insert
 	// http://localhost:7080/projectPMS/notice.do?method=insert
 	@GetMapping(params="method=insert")
