ac86eac522450a6e075275de39ade6fa514a764e
Isukim <aftersoul1994@gmail.com>
2021-04-07 19:02:49 +0900

    notice test 수정

--- src/pms/a02_service/A15_NoticeService.java
+++ src/pms/a02_service/A15_NoticeService.java
@@ -1,5 +1,19 @@
 package pms.a02_service;
 
-public class A15_NoticeService {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pms.a03_dao.A15_NoticeDao;
+import pms.z01_vo.Notice;
+
+@Service
+public class A15_NoticeService {
+	@Autowired(required = false)
+	private A15_NoticeDao daoNotice;
+
+	public ArrayList<Notice> getNoticeList(Notice sch) {
+		return daoNotice.getNoticeList(sch);
+	}
 }
