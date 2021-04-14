a1ad0695d2d221c08dc5073b52a72316ede134dd
kwonmyoungji <ji39098578@gmail.com>
2021-04-14 14:36:43 +0900

    journal vo수정

--- src/pms/z01_vo/Journal.java
+++ src/pms/z01_vo/Journal.java
@@ -5,18 +5,31 @@ import java.util.Date;
 public class Journal {
 	private int id;
 	private String document_type;
-	private int document_id;
+	private int document_id; 
+	private String content;
 	private int account_id;
-	private String created_on;
+//	private String created_on;
+	private Date created_on;
 	public Journal() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Journal(int id, String document_type, int document_id, int account_id, String created_on) {
+	/*
+	public Journal(int id, String document_type, int document_id, int account_id, Date created_on) {
 		super();
 		this.id = id;
 		this.document_type = document_type;
 		this.document_id = document_id;
+		this.account_id = account_id;
+		this.created_on = created_on;
+	}
+	*/
+	public Journal(int id, String document_type, int document_id, String content, int account_id, Date created_on) {
+		super();
+		this.id = id;
+		this.document_type = document_type;
+		this.document_id = document_id;
+		this.content = content;
 		this.account_id = account_id;
 		this.created_on = created_on;
 	}
@@ -38,17 +51,33 @@ public class Journal {
 	public void setDocument_id(int document_id) {
 		this.document_id = document_id;
 	}
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
 	public int getAccount_id() {
 		return account_id;
 	}
 	public void setAccount_id(int account_id) {
 		this.account_id = account_id;
 	}
+	/*
 	public String getCreated_on() {
 		return created_on;
 	}
 	public void setCreated_on(String created_on) {
 		this.created_on = created_on;
 	}
+	*/
+	public Date getCreated_on() {
+		return created_on;
+	}
+	public void setCreated_on(Date created_on) {
+		this.created_on = created_on;
+	}
+	
 
 }
