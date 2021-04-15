3ce6797d0577c2c6287bc2404ce51f8d0da3a9fe
chaeyoung_lee <www970630@gmail.com>
2021-04-15 20:25:11 +0900

    activity, settings

--- src/pms/a01_controller/A11_ActivityController.java
+++ src/pms/a01_controller/A11_ActivityController.java
@@ -1,13 +1,23 @@
 package pms.a01_controller;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.LocaleResolver;
 
+import pms.a02_service.A01_ProjectService;
 import pms.a02_service.A11_ActivityService;
 import pms.z01_vo.Journal;
+import pms.z01_vo.Project;
+import pms.z02_util.SessionManager;
 
 
 @Controller
@@ -16,10 +26,22 @@ public class A11_ActivityController {
 	@Autowired(required = false)
 	private A11_ActivityService service;
 	
+	@Autowired(required = false) 
+	private A01_ProjectService serviceProject;
+	
+	@Autowired(required = false)
+	private LocaleResolver localeResolver;
+	
 	
 	// http://localhost:7080/projectPMS/activity.do
 	@RequestMapping("activity.do")
-	public String Activity(@ModelAttribute("jr") Journal jr, Model d) {
+	public String Activity(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("jr") Journal jr, Model d) {
+		
+		/* Set Locale */
+		if (request.getParameter("lang") != null) {
+			SessionManager.setLang(request, response, localeResolver);
+		}
+		
 		d.addAttribute("activity", service.getJournalList(jr));
 
 		return "a11_activity\\activity";
--- src/pms/a01_controller/A17_SettingsController.java
+++ src/pms/a01_controller/A17_SettingsController.java
@@ -1,15 +1,20 @@
 package pms.a01_controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
 
 import pms.a02_service.A01_ProjectService;
 import pms.a02_service.A17_SettingsService;
 import pms.z01_vo.Project;
+import pms.z02_util.SessionManager;
 
 @Controller
 @RequestMapping("settings.do")
@@ -19,31 +24,54 @@ public class A17_SettingsController {
 	private A17_SettingsService service;
 	@Autowired(required = false)
 	private A01_ProjectService pservice;
+	@Autowired(required = false)
+	private LocaleResolver localeResolver;
 	
 	// http://localhost:7080/projectPMS/settings.do?method=project
 	@RequestMapping( params = "method=project")
-	public String Project(@ModelAttribute("sch")Project sch, Model d) {
+	public String Project(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("sch")Project sch, Model d) {
+		/* Set Locale */
+		if (request.getParameter("lang") != null) {
+			SessionManager.setLang(request, response, localeResolver);
+		}
+		
 		d.addAttribute("projectList", pservice.projectList(sch));
 		return "a17_settings\\a00_project";
 	}
 	// http://localhost:7080/projectPMS/settings.do?method=detail
 	@RequestMapping(params = "method=detail")
-	public String detail(@RequestParam("id") int id, Model d) {
+	public String detail(HttpServletRequest request, HttpServletResponse response, @RequestParam("id") int id, Model d) {
 		//System.out.println("id:"+id);
+		
+		/* Set Locale */
+		if (request.getParameter("lang") != null) {
+			SessionManager.setLang(request, response, localeResolver);
+		}
+		
 		d.addAttribute("project", pservice.getProject(id));
 		
 		return "a17_settings\\a01_update";
 	}
 	// http://localhost:7080/projectPMS/settings.do?method=update
 	@RequestMapping( params = "method=update")
-	public String update(Project upt) {
+	public String update(HttpServletRequest request, HttpServletResponse response, Project upt) {
+		/* Set Locale */
+		if (request.getParameter("lang") != null) {
+			SessionManager.setLang(request, response, localeResolver);
+		}
+		
 		service.projectUpt(upt);
 		return "forward:/settings.do?method=detail";
 	}
 	
 	// http://localhost:7080/projectPMS/settings.do?method=delete
 	   @RequestMapping(params="method=delete")
-	   public String delete(@RequestParam("id") int id) {
+	   public String delete(HttpServletRequest request, HttpServletResponse response, @RequestParam("id") int id) {
+		   /* Set Locale */
+			if (request.getParameter("lang") != null) {
+				SessionManager.setLang(request, response, localeResolver);
+			}
+		   
 		   service.projectDel(id);
 		   return "a17_settings\\a01_update";
 	   }
