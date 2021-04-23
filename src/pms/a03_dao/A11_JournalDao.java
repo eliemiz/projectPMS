1929c20a4581731dd556b99c8edf41b93906c0ba
chaeyoung_lee <www970630@gmail.com>
2021-04-23 17:20:11 +0900

    journal

--- src/pms/a03_dao/A11_JournalDao.java
+++ src/pms/a03_dao/A11_JournalDao.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import pms.z01_vo.Journal;
 import pms.z01_vo.JournalEx;
+import pms.z01_vo.JournalSch;
 
 @Repository
 public interface A11_JournalDao {
@@ -13,8 +14,8 @@ public interface A11_JournalDao {
 	public void insertJournal(Journal ins); // Journal 등록
 	/* public ArrayList<Journal> getJournalList(Journal jr); */
 	
-	public ArrayList<JournalEx> getJournalListAll(int project_id);
-	public ArrayList<JournalEx> getJournalListTask(int project_id);
-	public ArrayList<JournalEx> getJournalListRisk(int project_id);
+	public ArrayList<JournalEx> getJournalListAll(JournalSch sch);
+	public ArrayList<JournalEx> getJournalListTask(JournalSch sch);
+	public ArrayList<JournalEx> getJournalListRisk(JournalSch sch);
 	
 }
--- src/pms/z01_vo/JournalSch.java
+++ src/pms/z01_vo/JournalSch.java
@@ -3,30 +3,23 @@ package pms.z01_vo;
 public class JournalSch {
 
 private int projectId;
-private String document_type;
 private int document_id;
-	public JournalSch(int projectId, String document_type, int document_id) {
-		super();
-		this.projectId = projectId;
-		this.document_type = document_type;
-		this.document_id = document_id;
-	}
-	public int getProjectId() {
-		return projectId;
-	}
-	public void setProjectId(int projectId) {
-		this.projectId = projectId;
-	}
-	public String getDocument_type() {
-		return document_type;
-	}
-	public void setDocument_type(String document_type) {
-		this.document_type = document_type;
-	}
-	public int getDocument_id() {
-		return document_id;
-	}
-	public void setDocument_id(int document_id) {
-		this.document_id = document_id;
-	}
+public JournalSch(int projectId, int document_id) {
+	super();
+	this.projectId = projectId;
+	this.document_id = document_id;
+}
+public int getProjectId() {
+	return projectId;
+}
+public void setProjectId(int projectId) {
+	this.projectId = projectId;
+}
+public int getDocument_id() {
+	return document_id;
+}
+public void setDocument_id(int document_id) {
+	this.document_id = document_id;
+}
+
 }
